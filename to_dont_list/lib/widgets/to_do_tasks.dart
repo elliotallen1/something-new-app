5cd5c9a32fac5371122f1181666eebec5954b298
elliotallen1 <allened@hendrix.edu>
2024-09-16 22:50:59 -0500

    AWESOME style and theme
    Also really like the ListTiles here. Looks great.

--- to_dont_list/lib/widgets/to_do_tasks.dart
+++ to_dont_list/lib/widgets/to_do_tasks.dart
@@ -20,52 +20,66 @@ class ToDoListTask extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ListTile(
-        onTap: !completed 
-          ? () {
-            onListChanged(task, completed);
-          }
-          : null,
-        onLongPress: !completed
-          ? () {
-            onDeleteTask(task);
-          }
-          : null,
-        title: Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            task.name,
-            textAlign: TextAlign.left,
-          ),
-        ),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (task.rating != null) ...[
-              const SizedBox(height: 8),
-              Text(
-                'Rating: ${task.rating}/5',
-                textAlign: TextAlign.left,
-              ),
-            ],
-            if (task.description != null && task.description!.isNotEmpty) ...[
-              const SizedBox(height: 8),
-              Text(
-                'Description: ${task.description}',
-                textAlign: TextAlign.left,
-              ),
-            ],
-            if (task.wouldDoAgain != null) ...[
-              const SizedBox(height: 8),
-              Text(
-                'Would do again: ${task.wouldDoAgain! ? "Yes" : "No"}',
-                textAlign: TextAlign.left,
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface.withOpacity(0.80),
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(255, 206, 110, 209),
+            ),
           ],
         ),
-        trailing: completed ? const Icon(Icons.check_circle) : null,
+        child: ListTile(
+          contentPadding: const EdgeInsets.all(16.0),
+          onTap: !completed
+              ? () {
+                  onListChanged(task, completed);
+                }
+              : null,
+          onLongPress: !completed
+              ? () {
+                  onDeleteTask(task);
+                }
+              : null,
+          title: Text(
+            task.name,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              color: Theme.of(context).colorScheme.onSurface,
+              decoration: completed ? TextDecoration.lineThrough : null,
+            ),
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (task.rating != null) ...[
+                const SizedBox(height: 8),
+                Text(
+                  'Rating: ${task.rating}/5',
+                ),
+              ],
+              if (task.description != null && task.description!.isNotEmpty) ...[
+                const SizedBox(height: 8),
+                Text(
+                  'Description: ${task.description}',
+                ),
+              ],
+              if (task.wouldDoAgain != null) ...[
+                const SizedBox(height: 8),
+                Text(
+                  'Would do again: ${task.wouldDoAgain! ? "Yes" : "No"}',
+                ),
+              ],
+            ],
+          ),
+          trailing: completed
+              ? const Icon(Icons.check_circle, color: Color.fromARGB(255, 240, 164, 221))
+              : const Icon(Icons.circle_outlined, color: Colors.grey),
+        ),
       ),
     );
   }
